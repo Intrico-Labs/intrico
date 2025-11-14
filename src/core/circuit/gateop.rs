7d7c73f153f5d7af90d3847be50ffe81aa3126c6
Soham Ghugare <soham.ghugare@gmail.com>
2025-11-14 12:39:09 +0530

    fix: fixed gate type for gateop

--- src/core/circuit/gateop.rs
+++ src/core/circuit/gateop.rs
@@ -1,6 +1,13 @@
-use crate::core::gate::Gate;
+use crate::core::{QuantumGate};
 
 pub struct GateOp {
-    gate: Gate,
+    gate: QuantumGate,
     targets: Vec<usize>,
 }
+
+impl GateOp {
+    pub fn new(gate: QuantumGate, targets: Vec<usize>) -> Self {
+        GateOp { gate, targets }
+    }
+}
+
