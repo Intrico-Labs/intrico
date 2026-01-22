6e6b90520fb568c43f6d1ec83702eb666f5b951f
Soham Ghugare <soham.ghugare@gmail.com>
2026-01-23 00:03:33 +0530

    feat: added qft example

---
+++ examples/qft.rs
@@ -0,0 +1,19 @@
+use intrico::{circuit::QuantumCircuit, library::qft::QFT, state::QuantumState};
+
+fn main() {
+    // Initializing the circuit to |110⟩ state
+    let mut qc = QuantumCircuit::new(3);
+    qc.x(2).x(1);
+
+    // Appending the QFT circuit
+    let qft = QFT::new(3);
+    qc.append(&qft);
+
+    let mut statevec = QuantumState::new(3);
+
+    println!("Statevec before execution: {:?}", statevec.statevector);
+
+    qc.execute_on_state(&mut statevec);
+
+    println!("Statevec after execution: {:.4?}", statevec.statevector);
+}
