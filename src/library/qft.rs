6535961f5165a8c0e41fd8e5d006c31fd07e1ebd
Soham Ghugare <soham.ghugare@gmail.com>
2026-01-22 23:37:05 +0530

    feat: added qft integration

--- src/library/qft.rs
+++ src/library/qft.rs
@@ -1,3 +1,5 @@
+use std::f64::consts::PI;
+
 use crate::circuit::QuantumCircuit;
 
 
@@ -8,8 +10,19 @@ impl QFT {
     pub fn new(num_qubits: usize) -> QuantumCircuit {
         let mut qc = QuantumCircuit::new(num_qubits);
 
-        
+        for i in (0..num_qubits).rev() {
+            for k in ((i+1)..num_qubits).rev() {
+                let exp = (2 as u32).pow((k-1) as u32);
+                qc.cp(k, i, PI / exp as f64);
+            }
 
+            qc.h(i);
+        }
+
+        // swap gates for correct order
+        for i in 0..num_qubits / 2 {
+            qc.swap(i, num_qubits - 1 - i);
+        }
 
         qc
     }
